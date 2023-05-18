0d8837f5d2e3d0e99945b2dbd86aea6b50227ea2
NM10 <nahuemiglio@gmail.com>
2023-05-18 19:10:41 -0300

    Agregado archivo 'Tests.hs' y cambios menores

--- Solucion.hs
+++ Solucion.hs
@@ -121,7 +121,7 @@ compararCantidadDeAmigos red us | cantidadDeAmigos red u1 <= cantidadDeAmigos re
 
 -- Dada una red, valida si hay un usuario con más de un millón de amigos.
 estaRobertoCarlos :: RedSocial -> Bool
-estaRobertoCarlos red = (cantidadDeAmigos red (usuarioConMasAmigos red)) > 1000000000
+estaRobertoCarlos red = (cantidadDeAmigos red (usuarioConMasAmigos red)) > 10
 
 --------------------------------------------------- Problema 6
 
@@ -168,6 +168,7 @@ tieneUnSeguidorFiel red u = algunoLeDioLikeATodasPublicaciones red (publicacione
 -- Dada una red, una lista de publicaciones y una lista de usuarios, verifica si toda publicación de la lista de publicaciones pertenece a la lista de publicaciones que le gustan a algún usuario en la lista de usuarios.
 algunoLeDioLikeATodasPublicaciones :: RedSocial -> [Publicacion] -> [Usuario] -> Bool
 algunoLeDioLikeATodasPublicaciones _ _ [] = False
+algunoLeDioLikeATodasPublicaciones _ [] _ = False
 algunoLeDioLikeATodasPublicaciones red pubs us = (todoElemPertenece pubs (publicacionesQueLeGustanA red u)) || (algunoLeDioLikeATodasPublicaciones red pubs usres)
                                                  where u = head(us)
                                                        usres = tail(us)
@@ -402,50 +403,3 @@ sinRepetidos l = not (pertenece hl tl) && (sinRepetidos tl)
                        tl = tail(l)
 
 ----------------------------------------------------------------------------------------------------
-
--------------------------------------------------- Variables de Tests 
-
--- usuario1 = (1, "Juan")
--- usuario2 = (2, "Natalia")
--- usuario3 = (3, "Pedro")
--- usuario4 = (4, "Mariela")
--- usuario5 = (5, "Natalia")
--- usuario6 = (6, "Juan")
--- usuario7 = (7, "Carlos")
--- usuario8 = (8, "Pepe")
-
--- relacion1_2 = (usuario1, usuario2)
--- relacion1_3 = (usuario1, usuario3)
--- relacion1_4 = (usuario4, usuario1) 
--- relacion2_3 = (usuario3, usuario2)
--- relacion2_4 = (usuario2, usuario4)
--- relacion3_4 = (usuario4, usuario3)
-
--- publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])
--- publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
--- publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
--- publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
--- publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
-
--- publicacion2_1 = (usuario2, "Hello World", [usuario4])
--- publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
-
--- publicacion3_1 = (usuario3, "Lorem Ipsum", [])
--- publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
--- publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
-
--- publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
--- publicacion4_2 = (usuario4, "I am Bob", [])
--- publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
-
--- usuariosA = [usuario1, usuario2, usuario3, usuario4]
--- relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4]
--- publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2]
--- redA = (usuariosA, relacionesA, publicacionesA)
-
--- usuariosB = [usuario1, usuario2, usuario3, usuario5]
--- relacionesB = [relacion1_2, relacion2_3]
--- publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
--- redB = (usuariosB, relacionesB, publicacionesB)
-
--- usuariosC = [usuario1, usuario2, usuario3, usuario4, usuario5, usuario6, usuario7, usuario8]
